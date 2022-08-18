2b22338995b3b91e5f81fafc370fe5e022b62e7d
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-08-18 10:27:11 -0300

    [Feature] Migrated EndpointFactory

---
+++ MamisSolidarias.Utils.Test/EndpointFactory.cs
@@ -0,0 +1,55 @@
+﻿using System.Security.Claims;
+using FastEndpoints;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MamisSolidarias.Utils.Test;
+
+public static class EndpointFactory
+{
+    
+    public static EndpointConfiguration<TEndpoint> CreateEndpoint<TEndpoint>(params object?[] constructorParameters) 
+        where TEndpoint : class, IEndpoint
+    {
+        return new EndpointConfiguration<TEndpoint>(constructorParameters);
+    }
+}
+
+public class EndpointConfiguration<TEndpoint> where TEndpoint : class, IEndpoint
+{
+    public EndpointConfiguration(object?[] constructorParameters)
+    {
+        ConstructorParameters = constructorParameters;
+    }
+    private object?[] ConstructorParameters { get; }
+    private ClaimsPrincipal? UserClaims { get; set; }
+    private Action<ServiceCollection>? DependencyInjector { get; set; }
+    
+    public EndpointConfiguration<TEndpoint> WithInjectedServices(Action<ServiceCollection> services)
+    {
+        DependencyInjector = services;
+        return this;
+    }
+
+    public EndpointConfiguration<TEndpoint> WithClaims(ClaimsPrincipal claims)
+    {
+        UserClaims = claims;
+        return this;
+    }
+    
+    public TEndpoint Build() =>
+        Factory.Create<TEndpoint>(
+            ctx =>
+            {
+                if (UserClaims is not null)
+                    ctx.User = UserClaims;
+
+                if (DependencyInjector is not null)
+                {
+                    var services = new ServiceCollection();
+                    DependencyInjector.Invoke(services);
+                    ctx.RequestServices = services.BuildServiceProvider();
+                }
+            }, 
+            ConstructorParameters
+        );
+}
