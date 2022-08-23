1605f7a4ec216c15d94b2e874cfdc9a61c89cebe
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-08-23 09:56:51 -0300

    [Feature] Added more documentation to the methods

--- MamisSolidarias.Utils/Security/Services.cs
+++ MamisSolidarias.Utils/Security/Services.cs
@@ -1,13 +1,36 @@
 namespace MamisSolidarias.Utils.Security;
 
+/// <summary>
+/// A list of all the services on the network
+/// </summary>
 public enum Services
 {
+    /// <summary>
+    /// Users service
+    /// </summary>
     Users,
+    /// <summary>
+    /// Beneficiaries service
+    /// </summary>
     Beneficiaries
 }
 
+/// <summary>
+/// A list of extension methods for the services
+/// </summary>
 public static class ServiceExtensions
 {
+    /// <summary>
+    /// It calculates the read permission string
+    /// </summary>
+    /// <param name="service">Service to create the read permission string for</param>
+    /// <returns>The read permission string</returns>
     public static string ReadPermission(this Services service) => $"{service}/read";
+    
+    /// <summary>
+    /// It calculates the write permission string
+    /// </summary>
+    /// <param name="service">Service to create the write permission string for</param>
+    /// <returns>The write permission string</returns>
     public static string WritePermission(this Services service) => $"{service}/write";
 }
