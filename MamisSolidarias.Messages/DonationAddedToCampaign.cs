590f9cd500720ec799c6e374d965294cf970aa38
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-01-07 14:00:24 -0300

    [Feature] Created DonationAddedToCampaign message

---
+++ MamisSolidarias.Messages/DonationAddedToCampaign.cs
@@ -0,0 +1,16 @@
+namespace MamisSolidarias.Messages;
+
+public enum Campaigns
+{
+    UnaMochiComoLaTuya,
+    JuntosALaPar
+}
+
+/// <param name="DonationId">Id of the donation</param>
+/// <param name="ParticipantId">Id of the participant of the campaign</param>
+/// <param name="Campaign">Campaign of the participant</param>
+public sealed record DonationAddedToCampaign(
+    Guid DonationId,
+    int ParticipantId,
+    Campaigns Campaign
+);
