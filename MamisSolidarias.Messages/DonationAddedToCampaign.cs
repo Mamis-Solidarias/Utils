d26d1cc52ec98d2cb064bbc4106f69c02383c0f8
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-01-08 09:53:37 -0300

    [Feature] Added donorId field to DonationAddedToCampaign (#77)
    * [Feature] Added donorId field to DonationAddedToCampaign
    
    * [Chore] Bumped nuget version of Messages

--- MamisSolidarias.Messages/DonationAddedToCampaign.cs
+++ MamisSolidarias.Messages/DonationAddedToCampaign.cs
@@ -13,10 +13,12 @@ public enum Campaigns
 
 /// <param name="DonationId">Id of the donation</param>
 /// <param name="ParticipantId">Id of the participant of the campaign</param>
+/// <param name="DonorId">Id of the donor</param>
 /// <param name="CampaignId">Id of the campaign</param>
 /// <param name="Campaign">Campaign of the participant</param>
 public sealed record DonationAddedToCampaign(
     Guid DonationId,
+    int? DonorId,
     int? ParticipantId,
     int CampaignId,
     Campaigns Campaign
