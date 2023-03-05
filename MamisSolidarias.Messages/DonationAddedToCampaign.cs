1a55024e79153e3b645203a102cc3efe6bcfe152
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-03-05 18:09:59 +0100

    [Feature] Added currency and amount

--- MamisSolidarias.Messages/DonationAddedToCampaign.cs
+++ MamisSolidarias.Messages/DonationAddedToCampaign.cs
@@ -11,15 +11,26 @@ public enum Campaigns
     Misiones
 }
 
+public enum Currency
+{
+    ARS,
+    USD,
+    EUR
+}
+
 /// <param name="DonationId">Id of the donation</param>
 /// <param name="ParticipantId">Id of the participant of the campaign</param>
 /// <param name="DonorId">Id of the donor</param>
 /// <param name="CampaignId">Id of the campaign</param>
 /// <param name="Campaign">Campaign of the participant</param>
+/// <param name="Amount">Amount of donation</param>
+/// <param name="Currency">Currency of the donation</param>
 public sealed record DonationAddedToCampaign(
     Guid DonationId,
     int? DonorId,
     int? ParticipantId,
     int CampaignId,
-    Campaigns Campaign
+    Campaigns Campaign,
+    decimal Amount,
+    Currency Currency
 );
